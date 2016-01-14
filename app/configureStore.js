0a1ef2ab5e04b217ed7a717c82a567c37d11759b
Dominic Tobias <dominic.tobias@gmail.com>
2016-01-14 19:20:44 +0000

    Remove unused import

--- app/configureStore.js
+++ app/configureStore.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
-import { createHistory } from 'history';
 import rootReducer from './reducers';
 import routes from './routes';
 import { isClient } from './utils';
